2a354002f313f9bd0309c67e5851a4ff7dbdb99c
leeyang2019 <nyang@madisoncollege.edu>
2020-07-07 17:54:24 -0400

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/Users/Users';
 import axios from 'axios';
 import Search from './components/Users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 
 class App extends Component {
   state = {
@@ -42,19 +44,32 @@ class App extends Component {
   render() {
     const { users, loading } = this.state;
     return (
-      <div>
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search
-            searchUsers={this.searchUsers}
-            clearUsers={this.clearUsers}
-            showClear={users.length > 0 ? true : false}
-            setAlert={this.setAlert}
-          />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div>
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,18 +2,31 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 const Navbar = ({ icon, title }) => {
   return (
-    <AppBar position="absolute">
-      <Toolbar>
+    <AppBar
+      position="fixed"
+      style={{
+        backgroundColor: '#3f51b5',
+        padding: '5px 0',
+      }}
+    >
+      <Toolbar style={{ display: 'flex' }}>
         <IconButton edge="start" color="inherit" aria-label="menu">
           <i className={icon} />
         </IconButton>
         <Typography variant="h6">{title}</Typography>
+        <Link to="/">
+          <Button color="inherit">Home</Button>
+        </Link>
+        <Link to="/about">
+          <Button color="inherit">About</Button>
+        </Link>
       </Toolbar>
     </AppBar>
   );
